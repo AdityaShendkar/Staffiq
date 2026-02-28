a3bd976f63e46923322540ae202fe71c666d3ff2
Aditya Shendkar <aditya.shendkar03@gmail.com>
2026-02-28 13:44:46 +0530

    Added initial files

---
+++ src/main/java/com/staffiq/StaffiqApplication.java
@@ -0,0 +1,13 @@
+package com.staffiq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaffiqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StaffiqApplication.class, args);
+    }
+
+}
